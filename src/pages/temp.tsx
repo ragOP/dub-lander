cf22f01e37748f19758b7fbcac920b57f08590e8
ragOP <najmiraghib@gmail.com>
2024-10-19 08:39:20 +0530

    test2

--- src/pages/temp.tsx
+++ src/pages/temp.tsx
@@ -382,40 +382,54 @@ export default function Temp() {
         </div>
       ) : (
         <div className="checking">
-          <div className="congrats">Congratulations, You Qualify!</div>
-          <div className="top-description-5">
-            Make A <b>Quick Call</b> To Claim Your Food Allowance!
-          </div>
-          <div className="spots-count">Spots remaining: 4</div>
-          <div className="tap-direction">👇 TAP BELOW TO CALL 👇</div>
-          <a href="tel:+18444340339">             <div className="call-btn" onClick={handleCall}>             CALL (844) 434-0339}            </div>           </a>
-       <div className="sub-description">
-          Make sure to ask for medicare benefit for your area in order to receive the <b> Highest Possible Allowance.</b>
-          </div>
-          <div className="sub-title">We Have Reserved Your Spot</div>
-          <div className="sub-description">
-            Due to high call volume, your official agent is waiting for only{" "}
-            <b>3 minutes</b>, then your spot will not be reserved.
-          </div>
-          <div className="timer">
-            <div className="timer-cell">{min}</div>
-            <div className="timer-cell">:</div>
-            <div className="timer-cell">{second}</div>
-          </div>
+        <div className="congrats">Congratulations, You Pre-Qualify!</div>
+        <div className="top-description-5">
+          Make A <b>Quick Call</b> To Claim Your Food Allowance!
         </div>
+        <div className="spots-count">Spots remaining: 4</div>
+        <div className="tap-direction">👇 TAP BELOW TO CALL 👇</div>
+        <a href="tel:+18444340339">             <div className="call-btn" onClick={handleCall}>            CALL (844) 434-0339           </div>           </a>
+        <div className="sub-title">We Have Reserved Your Spot.</div>
+        <div className="sub-description">
+          Due to high call volume, your official agent is waiting for only{" "}
+          <b>3 minutes</b>, then your spot will not be reserved.
+        </div>
+        <div className="timer">
+          <div className="timer-cell">{min}</div>
+          <div className="timer-cell">:</div>
+          <div className="timer-cell">{second}</div>
+        </div>
+      </div>
       )}
       <div className="footer2">
-      <div className="terms2">
-          <Link to="/terms-and-conditions">Terms & Conditions</Link> | 
-          <Link to="/privacy-policy">Privacy Policy</Link>
+      <p>We represent Medicare Advantage HMO, PPO and PFFS organizations and stand-alone PDP prescription drug plans that are contracted with Medicare. Enrollment depends on the plan’s contract renewal."
+				</p>
+				<p>Not all plans offer all of these benefits. Benefits may vary by carrier and location. Limitations and exclusions may apply.
+				</p>
+				<p>Enrollment in Medicare/Medicare Advantage may be limited to certain times of the year unless you qualify for a Special Enrollment Period
+
+				</p>
+				<p>seniorsbenefitshub.org is not connected with or endorsed by the US government or Federal Medicare program. This website is a solicitation for insurance and Medicare Advantage, Part D or Medicare supplement insurance comparison services. Medicare supplement insurance is available to those age 65 or older enrolled in Medicare Parts A and B, and in some states to those under age 65 eligible to Medicare due to disability or end stage renal disease.
+				</p>
+				<p>We do not offer every plan available in your area. Currently we represent [insert number of organizations] organizations which offer [insert number of plans] products in your area. Please contact Medicare.gov, 1–800–MEDICARE, or your local State Health Insurance Program (SHIP) to get information on all of your options.</p>
+        <div className="terms2">
+
+        <Link to="/terms-and-conditions">Terms & Conditions</Link> | 
+        <Link to="/privacy-policy">Privacy Policy</Link>
         </div>
-        {/* <div className="terms2">Terms & Conditions | Privacy Policy</div> */}
+        <div>
+        
+        <hr/>
+        </div>
         <div className="copyright">
-          Copyright © 2024 - All right reserved Daily America Savings.
+          
+        
         </div>
+
+<p>G2 Licensed Agent : Gregory K. Teipelz</p>
         {/* <p>{zipCode} </p> */}
       </div>
-{/*       <ToastContainer
+      {/* <ToastContainer
         position="bottom-right"
         autoClose={5000}
         newestOnTop={false}
